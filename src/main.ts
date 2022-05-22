f8f4269b19cd293ed196864777593e29283982f0
murnana <murnana@users.noreply.github.com>
2022-05-22 13:43:57 +0900

    add Octokit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    core.debug('Hello!')
+    // Get GitHub Token
+    const token = core.getInput('github-token', {
+      required: true,
+      trimWhitespace: false
+    })
+    core.debug(`Get token: ${token}`)
+
+    // Get Octokit
+    const octoKit = github.getOctokit(token, {
+      log: {
+        debug: core.debug,
+        info: core.info,
+        warn: core.warning,
+        error: core.error
+      },
+      timeZone: 'Asia/Tokyo'
+    })
+    core.debug(`Get octoKit: ${octoKit}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
